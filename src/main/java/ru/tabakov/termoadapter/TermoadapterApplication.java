1663be1df59c33c3240bd6d8409436bfdbf831f1
Табаков Константин Андреевич <Tabakov.K.An@sberbank.ru>
2020-10-14 00:20:14 +0700

    Инициализация проекта

---
+++ src/main/java/ru/tabakov/termoadapter/TermoadapterApplication.java
@@ -0,0 +1,13 @@
+package ru.tabakov.termoadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermoadapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermoadapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tabakov/termoadapter/TermoadapterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tabakov.termoadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermoadapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
